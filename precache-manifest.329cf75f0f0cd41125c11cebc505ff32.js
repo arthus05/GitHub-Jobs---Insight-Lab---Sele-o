28013b9a6338b02842e145b30b4aee1483527204
Arthus <arthusvinicius@alu.ufc.br>
2020-08-17 19:46:16 -0300

    Updates

---
+++ precache-manifest.329cf75f0f0cd41125c11cebc505ff32.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1000eaf95f83c6406bd26b4bbeb161d1",
+    "url": "/githubjobsclone/index.html"
+  },
+  {
+    "revision": "a37b2f549cf81b778900",
+    "url": "/githubjobsclone/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "cf061f738b0d02bba28a",
+    "url": "/githubjobsclone/static/css/main.fec458a0.chunk.css"
+  },
+  {
+    "revision": "a37b2f549cf81b778900",
+    "url": "/githubjobsclone/static/js/2.52cd8908.chunk.js"
+  },
+  {
+    "revision": "51d7567eecdbcda36c30bd3f2c6876f1",
+    "url": "/githubjobsclone/static/js/2.52cd8908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf061f738b0d02bba28a",
+    "url": "/githubjobsclone/static/js/main.3223272f.chunk.js"
+  },
+  {
+    "revision": "9fa5b78600a507bff981",
+    "url": "/githubjobsclone/static/js/runtime-main.3117a293.js"
+  },
+  {
+    "revision": "24d4513a03bacb823c06aab35322d798",
+    "url": "/githubjobsclone/static/media/1024px-Feed-icon.svg.24d4513a.png"
+  },
+  {
+    "revision": "acc37e384a2513c6ddf66384105365fd",
+    "url": "/githubjobsclone/static/media/190-1903304_twitter-t-icon-logo-vector-twitter-t-logo-vector.acc37e38.png"
+  },
+  {
+    "revision": "5e17e1abf9da1943f87668d54bf69922",
+    "url": "/githubjobsclone/static/media/Capturar.5e17e1ab.PNG"
+  },
+  {
+    "revision": "76383dfa25cd1cf632397f028a9a9b89",
+    "url": "/githubjobsclone/static/media/close.76383dfa.svg"
+  }
+]);
